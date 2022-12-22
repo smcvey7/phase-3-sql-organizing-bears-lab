31b53efa6490eecd2c9f528fcafebbe942d3cb99
Steven McVey <smcvey1210@gmail.com>
2022-12-22 00:42:21 -0800

    commit tree 66024510e3ffb801be49e6ed7248c18b4367c073

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Mr. Chocolate", 10, "M", "brown", "angry", "Y");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Rowdy", 10, "M", "brown", "angry", "Y");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Tabitha", 10, "M", "brown", "angry", "Y");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Sergeany Brown", 10, "M", "brown", "angry", "Y");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Melissa", 10, "M", "brown", "angry", "Y");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Grinch", 10, "F", "brown", "angry", "Y");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES("Wendy", 10, "M", "brown", "angry", "Y");
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES(NULL, 10, "F", "brown", "angry", "Y");
